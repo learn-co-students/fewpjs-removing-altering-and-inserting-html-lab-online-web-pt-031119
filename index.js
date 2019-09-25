cc9310273bf1a31169812a10b4efd63bc36f5749
Ryan <kerneltux0@gmail.com>
2019-09-25 12:18:14 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let main = document.getElementById("main");
+main.remove();
+let header = document.createElement('h1');
+document.body.appendChild(header);
+header.setAttribute('id', 'victory');
+
+let newHeader = document.querySelector('h1#victory');
+header.innerHTML = 'YOUR-NAME is the champion';
