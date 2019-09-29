227992d2bdd0659250152fbd6b23939f28fd9b22
github username <github email address>
2019-09-28 22:00:56 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+// no longer has DOM node 'main#main
+
+main = document.querySelector('main#main');
+main.remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Lex is the champion'
