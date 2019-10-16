0dc19c986d315b0e2ea91a34f62cb6d11621b0d7
David Acosta <davidacosta25081@yahoo.com>
2019-10-16 01:00:24 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var main = document.getElementById('main');
 main.remove();
 
 var newHeader = document.createElement('h1');
+newHeader.id = "victory";
 newHeader.setAttribute = ("victory", "H1");
 
 newHeader.textContent = 'David is the champion';
